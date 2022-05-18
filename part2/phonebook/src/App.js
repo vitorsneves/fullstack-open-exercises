3cd2d20e8543146498bb70057911192f3b93a5b0
vitorsneves <vitorneves20@outlook.com>
2022-05-18 09:56:01 -0300

    feature: finish phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import AddPeopleForm from "./components/AddPeopleForm";
 import PeopleDisplay from "./components/PeopleDisplay";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456" },
-    { name: "Ada Lovelace", phone: "39-44-5323523" },
-    { name: "Dan Abramov", phone: "12-43-234345" },
-    { name: "Mary Poppendieck", phone: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    const getPersons = async () => {
+      const response = await axios.get("http://localhost:3001/persons");
+      setPersons(response.data);
+    };
+
+    getPersons();
+  }, []);
 
   const addNewPerson = (newPerson) => {
     if (isPersonAlreadyAdded(newPerson)) {
