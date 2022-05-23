f128e2ccd4fc717102704ba04457ef219f07ff91
vitorsneves <vitorneves20@outlook.com>
2022-05-23 13:38:47 -0300

    phonebook api step 3

---
+++ part3/phonebook/index.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const app = express();
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <div>
+      <p>Phonebook has info for ${persons.length} people. </p>
+      <p>${new Date().toUTCString()}</p>
+    </div>
+  `);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    return response.status(404).end();
+  }
+
+  response.json(person);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
